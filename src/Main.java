ecfdbaba94fae25d7ab1b279f7a102fd744a4379
Anna <foxy2ears@gmail.com>
2026-01-15 20:22:31 +0300

    Fix variable and method naming

--- src/Main.java
+++ src/Main.java
@@ -2,16 +2,16 @@ import static java.lang.System.out;
 
 public class Main {
     public static void main(String[] args) {
-        int Balance = 100;
-        int Amount = 1100;
-        int bonusUp = 1000;
-        int rublesForHundred = 1;
+        int balance = 100;
+        int amount = 1100;
+        int bonusup = 1000;
+        int rublesforhundred = 1;
         int bonus = 0;
-        if (Amount > bonusUp) {
-            bonus = Amount / 100;
+        if (amount > bonusup) {
+            bonus = amount / 100;
         }
-        int finalBalance = Balance + Amount + bonus;
-        System.out.println("Итоговый счёт: " + finalBalance);
+        int finalbalance = balance + amount + bonus;
+        System.out.println("Итоговый счёт: " + finalbalance);
         System.out.println("Бонус: " + bonus);
     }
 }
