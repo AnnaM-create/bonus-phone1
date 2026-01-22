e6f71f268a94e64f02663c50a4048ecda3e1507c
Anna <foxy2ears@gmail.com>
2026-01-22 20:22:23 +0300

    Final version: BonusService with correct naming and clean repo

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,17 @@
-import static java.lang.System.out;
+
 
 public class Main {
     public static void main(String[] args) {
         int balance = 100;
         int amount = 1100;
-        int bonusup = 1000;
-        int rublesforhundred = 1;
+        int bonusUp = 1000;
+        int rublesForHundred = 1;
         int bonus = 0;
-        if (amount > bonusup) {
+        if (amount > bonusUp) {
             bonus = amount / 100;
         }
-        int finalbalance = balance + amount + bonus;
-        System.out.println("Итоговый счёт: " + finalbalance);
+        int finalBalance = balance + amount + bonus;
+        System.out.println("Итоговый счёт: " + finalBalance);
         System.out.println("Бонус: " + bonus);
     }
 }
